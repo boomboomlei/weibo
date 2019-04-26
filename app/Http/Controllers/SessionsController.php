a3dd7d1b6d9dc319c4f86161c2d4902ae4a11177
Your Nameboomboomlei <1846352100@qq.com>
2019-04-26 14:39:49 +0000

    访问授权

--- app/Http/Controllers/SessionsController.php
+++ app/Http/Controllers/SessionsController.php
@@ -7,6 +7,12 @@ use Auth;
 
 class SessionsController extends Controller
 {
+
+	public function __construct(){
+		$this->middleware('guest',[
+			'only'=>['create']
+		]);
+	}
     //
     public function create(){
     	return view('sessions.create');
@@ -23,7 +29,9 @@ class SessionsController extends Controller
 
        if(Auth::attempt($credentials,$request->has('remember'))){
        		session()->flash('success','欢迎回来！');
-       		return redirect()->route('users.show',[Auth::user()]);
+       		$fallback=route('users.show',Auth::user());
+       		return redirect()->intended($fallback);
+       		// return redirect()->route('users.show',[Auth::user()]);
        }else{
        		session()->flash('danger','抱歉，你的邮箱密码不匹配');
        		return redirect()->back()->withInput();
