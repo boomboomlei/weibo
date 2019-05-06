b8d447da13e70e0463c8c509a18f845627dfd367
Your Nameboomboomlei <1846352100@qq.com>
2019-05-06 12:54:53 +0000

    社交统计信息

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -5,12 +5,14 @@
 <!-- {{$user->name}}-{{$user->email}} -->
 <div class="row">
 	<div class="offset-md-2 col-md-8">
-		<div class="col-md-12">
-			<div class="offset-md-2 col-md-8">
+		
 				<section class="user_info">
 					@include('shared._user_info',['user'=>$user])
 				</section>
-
+				<section class="stats mt-2">
+					@include('shared._stats',['user'=>$user])
+				</section>
+				<hr>
 				<section class="status">
 					@if($statuses->count()>0)
 						<ul class="list-unstyled">
@@ -28,6 +30,5 @@
 
 			</div>
 		</div>
-	</div>
-</div>
+
 @stop
